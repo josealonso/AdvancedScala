89559b41f7d838ca808ef4b46f969402061059d7
josealonso <jose.alonso.programmer@gmail.com>
2023-09-28 12:35:22 +0200

    The "unapplySeq" method must be defined to use pattern matching on any sequence.

--- src/main/scala/lectures/AdvancedPatternMatching.scala
+++ src/main/scala/lectures/AdvancedPatternMatching.scala
@@ -46,6 +46,42 @@ object AdvancedPatternMatching extends App {
 
   println(mathProperty)
 
+  // infix patterns
+  case class Or[A, B](a: A, b: B)
+  val either = Or(2, "two")
+  val humanDescription = either match {
+    case number Or string => s"$number is written as $string"
+  }
+  println(humanDescription)
+
+  // decomposing sequences
+  val numbers = List(1,2,3,4)
+  val vararg = numbers match {
+    case List(1, _*) => "starting with 1"
+  }
+
+  abstract class MyList[+A] {
+    def head: A = ???
+    def tail: MyList[A] = ???
+  }
+  case object Empty extends MyList[Nothing]
+  case class Cons[+A](override val head: A, override val tail: MyList[A]) extends MyList[A]
+
+  object MyListPattern {
+    def unapplySeq[A](list: MyList[A]): Option[Seq[A]] =
+      if (list == Empty) Some(Seq.empty)
+      else unapplySeq(list.tail).map(list.head +: _)
+  }
+
+  val myList: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
+  val decomposed = myList match {
+    case MyListPattern(1, 2, _*) => "starting with 1, 2"
+    case _ => "something else"
+  }
+
+  println(decomposed)
+
+
 }
 
 
