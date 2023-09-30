6a346abacfbd81f58c93355642e0901cc6863a25
josealonso <jose.alonso.programmer@gmail.com>
2023-10-01 00:48:44 +0200

    Lazy vals might be used in filter and in for-comprehensions.

--- src/main/scala/lectures/LazyEvaluation.scala
+++ src/main/scala/lectures/LazyEvaluation.scala
@@ -43,4 +43,58 @@ object LazyEvaluation extends App {
   }
 
   println(byNameMethod_v2(retrieveMagicValue))
+
+  // filtering with lazy vals
+  def lessthan30(i: Int) = {
+    println(s"$i is less than 30?")
+    i < 30
+  }
+
+  def greaterthan20(i: Int) = {
+    println(s"$i is greater than 20?")
+    i > 20
+  }
+
+  val numbers = List(1, 25, 40, 5, 23)
+  val lt30 = numbers.filter(lessthan30)
+  val gt20 = lt30.filter(greaterthan20)
+  println(gt20)
+
+  // "withFilter" uses lazy vals under the hood
+  val lt30lazy = numbers.withFilter(lessthan30)
+  val gt20lazy = lt30lazy.withFilter(greaterthan20)
+//  println(gt20lazy)  // Not in order
+  gt20lazy.foreach(println)  // Printing it in order
+
+  // for-comprehensions use withFilter with guards
+  for {
+    a <- List(1,2,3) if a % 2 == 0
+  } yield a + 1
+  // The compiler translates it to
+  // List(1,2,3).withFilter(_ % 2 == 0).map(_ + 1)
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
