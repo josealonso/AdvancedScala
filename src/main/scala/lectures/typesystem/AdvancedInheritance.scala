142d1466220d3e2d054ad9b48a88fc9c34852a10
josealonso <jose.alonso.programmer@gmail.com>
2023-10-06 16:31:34 +0200

    Idem

--- src/main/scala/lectures/typesystem/AdvancedInheritance.scala
+++ src/main/scala/lectures/typesystem/AdvancedInheritance.scala
@@ -1,5 +1,36 @@
 package lectures.typesystem
 
-object AdvancedInheritance {
+object AdvancedInheritance extends App {
+
+  // 1.- convenience
+  trait Writer[T] {
+    def write(value: T): Unit
+  }
+
+  trait Closeable {
+    def close(status: Int): Unit
+  }
+
+  trait GenericStream[T] {
+    def forEach(f: T => Unit): Unit
+  }
+
+  def processStream[T](stream: GenericStream[T] with Writer[T] with Closeable): Unit = {
+    stream.forEach(println)
+    stream.close(0)
+    // ............
+  }
+
+  // 2.- diamond problem --> In Scala, last override gets picked
+
+  // 3.- In the context of "type linearization",
+  // the "super" keyword refers to the type immediately to the left.
+
+  trait Color
+  trait Red extends Color
+  // Red = AnyRef with <Color>
+
+
+
 
 }
