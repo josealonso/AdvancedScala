d318bf7d77877ebcf485816e5f5bb90187b068d0
josealonso <jose.alonso.programmer@gmail.com>
2023-10-09 19:02:27 +0200

    Achieved a more expressive code using implicits.

--- src/main/scala/lectures/typesystem/HigherKindedTypes.scala
+++ src/main/scala/lectures/typesystem/HigherKindedTypes.scala
@@ -5,12 +5,17 @@ object HigherKindedTypes extends App {
   trait AHigherKindedType[F[_]]
 
   // Generic monad
-  trait Monad[F[_], A] {
+  trait Monad[F[_], A] {   // higher-kinded type class
     def flatMap[B](f: A => F[B]): F[B]
     def map[B](f: A => B): F[B]
   }
 
-  def multiply[F[_], A, B](ma: Monad[F, A], mb: Monad[F, B]): F[(A, B)] = {
+  implicit class MonadList[A](list: List[A]) extends Monad[List, A] {
+    override def flatMap[B](f: A => List[B]): List[B] = list.flatMap(f)
+    override def map[B](f: A => B): List[B] = list.map(f)
+  }
+
+  def multiply[F[_], A, B](implicit ma: Monad[F, A], mb: Monad[F, B]): F[(A, B)] = {
     for {
       a <- ma
       b <- mb
@@ -20,16 +25,11 @@ object HigherKindedTypes extends App {
     */
   }
 
-  class MonadList[A](list: List[A]) extends Monad[List, A] {
-    override def flatMap[B](f: A => List[B]): List[B] = list.flatMap(f)
-    override def map[B](f: A => B): List[B] = list.map(f)
-  }
-
   val monadList = new MonadList(List(1,2,3))
   monadList.flatMap(x => List(x, x + 1))
   monadList.map(_ * 2)
 
-  println(multiply(new MonadList(List(1,2)), new MonadList(List("a", "b"))))
+  println(multiply(List(1,2), List("a", "b")))
 }
 
 
