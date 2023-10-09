ea727a97fdd778e14e76ce027ac83487cacc1a54
josealonso <jose.alonso.programmer@gmail.com>
2023-10-09 16:58:26 +0200

    Solution using type classes.

--- src/main/scala/lectures/typesystem/FBoundedPolymorphism.scala
+++ src/main/scala/lectures/typesystem/FBoundedPolymorphism.scala
@@ -36,7 +36,8 @@ object FBoundedPolymorphism extends App {
 
   // Solution #2 - FBP + self-types
   object Solution2 {
-    trait Animal[A <: Animal[A]] { self: A =>
+    trait Animal[A <: Animal[A]] {
+      self: A =>
       def eat: List[Animal[A]]
     }
 
@@ -51,33 +52,91 @@ object FBoundedPolymorphism extends App {
 
   // Those solutions do not work for complex hierarchies (over one level of inheritance)
   object ProblemInComplexHierarchies {
-    trait Animal[A <: Animal[A]] { self: A =>
+    trait Animal[A <: Animal[A]] {
+      self: A =>
       def eat: List[Animal[A]]
     }
+
     trait Fish extends Animal[Fish]
+
     class Shark extends Fish {
       override def eat: List[Animal[Fish]] = ???
     }
 
     class Cod extends Fish {
-      override def eat: List[Animal[Fish]] = List(new Shark)  // Wrong, but compiles!
+      override def eat: List[Animal[Fish]] = List(new Shark) // Wrong, but compiles!
     }
   }
 
+  // Solution #3 - type classes
+  object SolutionWithTypeClasses {
+
+    trait Animal
+    trait CanEat[A] {
+      def eat(a: A): List[A]
+    }
+
+    class Dog extends Animal
+    object Dog {
+      implicit object DogsCanEat extends CanEat[Dog] {
+        override def eat(a: Dog): List[Dog] = List()
+      }
+    }
+
+    class Cat extends Animal
+    object Cat {
+      implicit object CatsCanEat extends CanEat[Dog] { // this error is detected
+        override def eat(a: Dog): List[Dog] = List() // when cat.eat is executed
+      }
+    }
+
+    implicit class CanEatOps[A](animal: A) {
+      def eat(implicit canEat: CanEat[A]): List[A] =
+        canEat.eat(animal)
+    }
+
+    val dog = new Dog
+    dog.eat
+    /*
+      converted to CanEatOps[Dog](dog).eat(Dog.DogsCanEat)
+      implicit value to pass to eat: Dog.DogsCanEat
+    */
+    val cat = new Cat
+    // cat.eat
+    // Caveat: too abstract solution, not very explicit
+  }
+
+  // Solution #4 - type classes
+  object SolutionWithTypeClassesMoreReadable {
+
+    trait Animal[A] {
+      def eat(a: A): List[A]
+    }
+
+    class Dog
+    object Dog {
+      implicit object DogAnimal extends Animal[Dog] {
+        override def eat(a: Dog): List[Dog] = List()
+      }
+    }
+
+    class Cat
+    object Cat {
+      implicit object CatAnimal extends Animal[Dog] {
+        override def eat(a: Dog): List[Dog] = List()
+      }
+    }
+
+    implicit class AnimalOps[A](animal: A) {
+      def eat(implicit animalTypeClassInstance: Animal[A]): List[A] =
+        animalTypeClassInstance.eat(animal)
+    }
+
+    val dog = new Dog
+    dog.eat
+    val cat = new Cat
+    cat.eat     // compile error!!
+  }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
